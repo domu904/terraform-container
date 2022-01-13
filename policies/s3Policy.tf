6b5bf9d96300995a584cbfec6cf7124dc7c2c216
otaku904 <domu904@gmail.com>
2022-01-12 20:33:30 -0600

    Update s3Policy.tf

--- policies/s3Policy.tf
+++ policies/s3Policy.tf
@@ -3,3 +3,7 @@ resource "aws_s3_bucket_policy" {
   group = s3_group.admin
 }
 
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.my_bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
