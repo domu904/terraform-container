9445a7041ace47bb72166569c67981e387c653ad
otaku904 <domu904@gmail.com>
2022-01-13 19:57:44 -0600

    Update s3Policy.tf

--- policies/s3Policy.tf
+++ policies/s3Policy.tf
@@ -7,3 +7,9 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.my_bucket.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "${var.bucket_name}"
+  force_destroy = "true"
+
+}
